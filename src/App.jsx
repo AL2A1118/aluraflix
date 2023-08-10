f8e5de63da1a650bb7d19af6a11156b84cb93479
Stalin Acurio <sta_vla411@hotmail.com>
2023-08-10 16:42:12 -0500

    add cotegory component to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Category from './components/Category'
 import Header from './components/Header'
 
 function App() {
   return (
     <>
       <Header />
+      <main className='flex flex-col gap-8 p-4'>
+        <Category title='Front End' color='bg-blue-500' />
+        <Category title='Back End' color='bg-green-500' />
+      </main>
     </>
   )
 }
