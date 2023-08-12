ca805a487978396970534be85a509730a54bae0b
Stalin <sta_vla411@hotmail.com>
2023-08-12 00:59:57 -0500

    add new category component

---
+++ src/components/NewCategory.jsx
@@ -0,0 +1,20 @@
+import Button from './Button'
+import Input from './Input'
+import TextArea from './TextArea'
+
+function NewCategory() {
+  return (
+    <section className='flex flex-col gap-4 '>
+      <h2 className='text-xl text-blue-400 font-medium'>Nuevo producto</h2>
+      <Input label='Título' />
+      <TextArea label='Descripción' />
+      <Input label='Usuario' />
+      <div className='flex gap-4'>
+        <Button name='Guardar' position='left' />
+        <Button name='Limpiar' />
+      </div>
+    </section>
+  )
+}
+
+export default NewCategory
