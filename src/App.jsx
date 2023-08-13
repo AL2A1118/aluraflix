d1de593b3b7b6a57c7fe3619172fbf59b80df0b2
Stalin <sta_vla411@hotmail.com>
2023-08-12 21:36:17 -0500

    add style in main

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import Button from './components/Button'
 import Category from './components/Category'
+import Footer from './components/Footer'
 import Header from './components/Header'
+import NewCategory from './components/NewCategory'
+import NewProduct from './components/NewProduct'
 
 function App() {
   return (
     <>
       <Header />
-      <main className='flex flex-col gap-8 p-4'>
-        <Category title='Front End' color='bg-blue-500' />
+      <main className='flex flex-col w-full justify-center gap-8 p-4 max-w-4xl mx-auto'>
+        {/* <Category title='Front End' color='bg-blue-500' /> */}
         <Category title='Back End' color='bg-green-500' />
+        {/* <NewProduct /> */}
+        {/* <NewCategory /> */}
+        <Button name='Añadir video' position='left' />
       </main>
+      <Footer />
     </>
   )
 }
