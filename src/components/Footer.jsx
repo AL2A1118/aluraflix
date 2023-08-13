b86f83bd3537ae43566a5c89f1d4561e0845aa2c
Stalin <sta_vla411@hotmail.com>
2023-08-12 21:35:46 -0500

    add margin top and button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className='border-t-4 border-blue-500 p-4 flex justify-center'>
+    <footer className='border-t-4 border-blue-500 p-4 flex justify-center mt-6'>
       <p>
         Reto de{' '}
         <a className='text-blue-500' href='https://www.aluracursos.com/'>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import logo from '../assets/logo.svg'
 function Header() {
   return (
-    <header className='flex justify-center items-center border-b-4 border-blue-700 p-4'>
+    <header className='flex justify-center items-center border-b-4 border-blue-500 p-4 mb-6'>
       <img className='w-40' src={logo} alt='' />
     </header>
   )
