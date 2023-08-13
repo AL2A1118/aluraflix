6985235e2583fcf6f66d14764939dc25426e02c4
Stalin <sta_vla411@hotmail.com>
2023-08-12 21:36:27 -0500

    update css

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,11 +1,11 @@
 function Category({ title, color }) {
   return (
-    <section className='flex flex-col gap-2'>
-      <div className={`${color} py-2 px-4 text-slate-200 rounded-lg w-max`}>
-        <h2 className='text-xl'>{title}</h2>
+    <section className='flex flex-col gap-2 max-w-4xl'>
+      <div className={`${color} py-1 px-3 text-slate-200 rounded-lg w-max`}>
+        <h2 className='text-lg'>{title}</h2>
       </div>
-      <p>Formacion {title} de Alura Latam</p>
-      <div className='w-40 h-40 rounded-lg bg-blue-300'></div>
+      <p>Formación {title} de Alura Latam</p>
+      <div className='w-48 h-48 rounded-lg bg-blue-300'></div>
     </section>
   )
 }
