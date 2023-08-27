0654a9c7426514b4b56587140eb7f9ead67dfc78
Stalin Acurio <sta_vla411@hotmail.com>
2023-08-26 23:49:17 -0500

    add value, name, onchange, and handle input change

--- src/components/NewProduct.jsx
+++ src/components/NewProduct.jsx
@@ -1,18 +1,53 @@
+import { useState } from 'react'
 import Button from './Button'
 import Input from './Input'
 import TextArea from './TextArea'
 
 function NewProduct() {
+  const [infoProduct, setInfoProduct] = useState({
+    title: '',
+    linkVideo: '',
+    linkImage: '',
+    user: ''
+  })
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setInfoProduct((prevState) => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+  const handleSubmit = () => {}
   return (
     <section className='flex flex-col gap-4 '>
       <h2 className='text-xl text-blue-400 font-medium'>Nuevo producto</h2>
-      <Input label='Título' />
-      <Input label='Link del video' />
-      <Input label='Link imagen del video' />
+      <Input
+        name='title'
+        label='Título'
+        value={infoProduct.title}
+        handleChange={handleInputChange}
+      />
+      <Input
+        name='linkVideo'
+        label='Link del video'
+        value={infoProduct.linkVideo}
+        handleChange={handleInputChange}
+      />
+      <Input
+        name='linkImage'
+        label='Link imagen del video'
+        value={infoProduct.linkImage}
+        handleChange={handleInputChange}
+      />
       <TextArea label='Descripción' />
-      <Input label='Usuario' />
+      <Input
+        name='user'
+        label='Usuario'
+        value={infoProduct.user}
+        handleChange={handleInputChange}
+      />
       <div className='flex gap-4'>
-        <Button name='Guardar' position='left' />
+        <Button name='Guardar' position='left' handleSubmit={handleSubmit} />
         <Button name='Limpiar' />
       </div>
     </section>
