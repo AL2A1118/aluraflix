5671bd2b595932dbf1889ca2bea5a9c805591edc
Stalin <sta_vla411@hotmail.com>
2023-09-17 22:43:00 -0500

    add path to link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,17 @@
-function Button({ name, position }) {
+import { Link } from 'react-router-dom'
+
+function Button({ name, position, path }) {
   return (
-    <button
+    <Link
       className={`px-4 py-2 ${
         position == 'left'
           ? 'bg-blue-500 text-gray-200'
           : 'bg-gray-300 text-gray-950 font-medium'
-      } w-full rounded-lg `}
+      } w-full rounded-lg flex justify-center`}
+      to={path}
     >
       {name}
-    </button>
+    </Link>
   )
 }
 
