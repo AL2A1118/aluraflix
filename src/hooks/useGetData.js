5d68fdc74786521365200fa378238e7bbb78fc04
Stalin <sta_vla411@hotmail.com>
2023-09-19 13:09:40 -0500

    add hook to set data from the api

---
+++ src/hooks/useGetData.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import { getDataApi } from '../services/fetchApi'
+
+function useGetData(url) {
+  const [data, setData] = useState()
+  const refreshData = () => {
+    getDataApi(url).then((data) => setData(data))
+  }
+
+  useEffect(() => {
+    refreshData()
+  }, [])
+
+  return { data, refreshData }
+}
+
+export default useGetData
