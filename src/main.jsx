85baad21b5cfd60b90f614a7211c0976428c9661
Stalin <sta_vla411@hotmail.com>
2023-09-19 13:10:03 -0500

    add context

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import App from './App.jsx'
 import './index.css'
 import '@fontsource-variable/roboto-flex'
 import { BrowserRouter } from 'react-router-dom'
+import ApiContext from './context/Api.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <ApiContext>
+      <App />
+    </ApiContext>
   </BrowserRouter>
 )
