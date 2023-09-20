b09994b464fc36180e89a83339790fbc1c16cb92
Stalin <sta_vla411@hotmail.com>
2023-09-20 11:48:37 -0500

    change styles

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,9 @@
 import Button from './Button'
 function Banner() {
   return (
-    <section className='relative h-[50vh] flex flex-col justify-end items-center [&>button]:w-auto gap-4'>
-      <h2 className='text-4xl text-slate-100'>Challenge React</h2>
-      <Button name='Ver' />
+    <section className='relative h-[50vh] flex flex-col justify-end items-center [&>a]:w-auto [&>a]:px-10 gap-4'>
+      <h2 className='text-3xl text-slate-100'>Challenge React</h2>
+      <Button name='Ver' to='/' />
       <div className='-z-10 opacity-50 bg-center absolute -left-4 w-screen h-[50vh] bg-banner bg-cover'></div>
     </section>
   )
