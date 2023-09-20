c0b93adf2688e4e387e8496aed673334d12b1bfc
Stalin <sta_vla411@hotmail.com>
2023-09-19 21:47:00 -0500

    add context of view categories

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,11 +1,31 @@
+import { useContext } from 'react'
+import { ApiContext } from '../context/Api'
+
 function Category({ title, color }) {
+  const { videos } = useContext(ApiContext)
   return (
     <section className='flex flex-col gap-2 max-w-4xl'>
-      <div className={`${color} py-1 px-3 text-slate-200 rounded-lg w-max`}>
+      <div
+        className='py-1 px-3 text-slate-200 rounded-lg w-max'
+        style={{ backgroundColor: color }}
+      >
         <h2 className='text-lg'>{title}</h2>
       </div>
       <p>Formación {title} de Alura Latam</p>
-      <div className='w-48 h-48 rounded-lg bg-blue-300'></div>
+      {videos &&
+        videos.map(({ imagen, categoria, id }) => {
+          if (categoria === title) {
+            return (
+              <img
+                style={{ borderColor: color }}
+                className='border-2 rounded'
+                key={id}
+                src={imagen}
+                alt='img'
+              />
+            )
+          }
+        })}
     </section>
   )
 }
