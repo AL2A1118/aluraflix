8efb6c69beb9b66cde9fa1ed50a9546e5c81f73f
Stalin <sta_vla411@hotmail.com>
2023-09-20 11:49:10 -0500

    move to slider component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,31 +1,16 @@
-import { useContext } from 'react'
-import { ApiContext } from '../context/Api'
+import Slider from './Slider'
 
 function Category({ title, color }) {
-  const { videos } = useContext(ApiContext)
   return (
-    <section className='flex flex-col gap-2 max-w-4xl'>
+    <section className='flex flex-col gap-2 max-w-4xl overflow-hidden'>
       <div
         className='py-1 px-3 text-slate-200 rounded-lg w-max'
         style={{ backgroundColor: color }}
       >
-        <h2 className='text-lg'>{title}</h2>
+        <h2>{title}</h2>
       </div>
-      <p>Formación {title} de Alura Latam</p>
-      {videos &&
-        videos.map(({ imagen, categoria, id }) => {
-          if (categoria === title) {
-            return (
-              <img
-                style={{ borderColor: color }}
-                className='border-2 rounded'
-                key={id}
-                src={imagen}
-                alt='img'
-              />
-            )
-          }
-        })}
+      <p className='text-sm'>Formación {title} de Alura Latam</p>
+      <Slider color={color} title={title} />
     </section>
   )
 }
