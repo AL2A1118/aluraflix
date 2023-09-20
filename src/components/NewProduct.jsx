9cce6c4425481cde3bd34299744a0a41deb348d1
Stalin <sta_vla411@hotmail.com>
2023-09-19 21:46:02 -0500

    add handle click and change

--- src/components/NewProduct.jsx
+++ src/components/NewProduct.jsx
@@ -6,6 +6,7 @@ import TextArea from './TextArea'
 function NewProduct() {
   const [infoProduct, setInfoProduct] = useState({
     title: '',
+    description: '',
     linkVideo: '',
     linkImage: '',
     user: ''
@@ -17,39 +18,58 @@ function NewProduct() {
       [name]: value
     }))
   }
-  const handleSubmit = () => {}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(infoProduct)
+  }
+  const handleClick = () => {
+    setInfoProduct({
+      title: '',
+      linkVideo: '',
+      linkImage: '',
+      user: '',
+      description: ''
+    })
+  }
   return (
     <section className='flex flex-col gap-4 '>
-      <h2 className='text-xl text-blue-400 font-medium'>Nuevo producto</h2>
-      <Input
-        name='title'
-        label='Título'
-        value={infoProduct.title}
-        handleChange={handleInputChange}
-      />
-      <Input
-        name='linkVideo'
-        label='Link del video'
-        value={infoProduct.linkVideo}
-        handleChange={handleInputChange}
-      />
-      <Input
-        name='linkImage'
-        label='Link imagen del video'
-        value={infoProduct.linkImage}
-        handleChange={handleInputChange}
-      />
-      <TextArea label='Descripción' />
-      <Input
-        name='user'
-        label='Usuario'
-        value={infoProduct.user}
-        handleChange={handleInputChange}
-      />
-      <div className='flex gap-4'>
-        <Button name='Guardar' position='left' handleSubmit={handleSubmit} />
-        <Button name='Limpiar' />
-      </div>
+      <form className='flex flex-col gap-4 ' action='submit'>
+        <h2 className='text-xl text-blue-400 font-medium'>Nuevo producto</h2>
+        <Input
+          name='title'
+          label='Título'
+          value={infoProduct.title}
+          handleChange={handleInputChange}
+        />
+        <Input
+          name='linkVideo'
+          label='Link del video'
+          value={infoProduct.linkVideo}
+          handleChange={handleInputChange}
+        />
+        <Input
+          name='linkImage'
+          label='Link imagen del video'
+          value={infoProduct.linkImage}
+          handleChange={handleInputChange}
+        />
+        <TextArea
+          name='description'
+          label='Descripción'
+          value={infoProduct.description}
+          handleInputChange={handleInputChange}
+        />
+        <Input
+          name='user'
+          label='Usuario'
+          value={infoProduct.user}
+          handleChange={handleInputChange}
+        />
+        <div className='flex gap-4'>
+          <Button name='Guardar' position='left' handleClick={handleSubmit} />
+          <Button name='Limpiar' handleClick={handleClick} />
+        </div>
+      </form>
       <div>
         <Button
           name='Nueva categoría'
