1c9c71e929618278bd771d016f4c545dd5513930
Stalin <sta_vla411@hotmail.com>
2023-09-19 21:46:15 -0500

    add props of textarea

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,13 +1,16 @@
-function TextArea({ label }) {
+function TextArea({ label, name, handleInputChange, value }) {
   return (
     <div className='flex flex-col bg-slate-600 rounded py-1 px-3'>
       <label className='text-sm' htmlFor=''>
         {label}
       </label>
       <textarea
+        name={name}
         className='bg-transparent focus-visible:outline-none text-slate-100'
         rows={5}
         type='text'
+        value={value}
+        onChange={handleInputChange}
       />
     </div>
   )
