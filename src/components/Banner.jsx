694a1dafdf31dac2d6c4ee8ef2483d75a83d1388
Stalin <sta_vla411@hotmail.com>
2023-09-20 23:03:53 -0500

    change styles

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,10 @@
 import Button from './Button'
 function Banner() {
   return (
-    <section className='relative h-[50vh] flex flex-col justify-end items-center [&>a]:w-auto [&>a]:px-10 gap-4'>
+    <section className='relative pb-4 h-[50vh] flex flex-col justify-end items-center [&>a]:w-auto [&>a]:px-10 gap-4'>
+      <div className='absolute inset-0 bg-banner bg-cover bg-center -left-4 w-screen opacity-30 z-[-1]'></div>
       <h2 className='text-3xl text-slate-100'>Challenge React</h2>
       <Button name='Ver' to='/' />
-      <div className='-z-10 opacity-50 bg-center absolute -left-4 w-screen h-[50vh] bg-banner bg-cover'></div>
     </section>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import logo from '../assets/logo.svg'
 function Header() {
   return (
-    <header className='flex justify-center items-center border-b-4 border-blue-500 p-4 mb-6'>
+    <header className='flex justify-center items-center border-b-4 border-blue-500 p-4'>
       <Link to='/'>
         <img className='w-40' src={logo} alt='' />
       </Link>
