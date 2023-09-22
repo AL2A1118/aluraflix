0e2510c84b6bade6d768bae25207997ea848afe6
Stalin <sta_vla411@hotmail.com>
2023-09-22 18:06:53 -0500

    remove dots and add media queries

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,43 +1,53 @@
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import { useState } from 'react'
 function Carousel({ color, listImage }) {
-  const [currentIndex, setCurrentIndex] = useState(0)
   const settings = {
-    dots: true,
-    customPaging: function (i) {
-      return (
-        <div
-          style={{
-            width: '10px',
-            height: '10px',
-            background: i === currentIndex ? color : '#ccc',
-            borderRadius: '50%'
-          }}
-        ></div>
-      )
-    },
+    dots: false,
     infinite: false,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 2.7,
     slidesToScroll: 1,
-    beforeChange: (oldIndex, newIndex) => setCurrentIndex(newIndex),
     swipeToSlide: true,
-    dotsClass: 'absolute bottom-4 w-full !flex justify-center gap-2',
-    arrows: false
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2.2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1.6,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   }
   return (
     <Slider {...settings}>
       {listImage &&
-        listImage.map(({ imagen, id }) => (
-          <li key={id} className='cursor-pointer'>
-            <img
-              className='border-2 rounded '
-              style={{ borderColor: color }}
-              src={imagen}
-              alt={`Image ${id}`}
-            />
+        listImage.map(({ imagen, id, video }) => (
+          <li key={id} className='cursor-pointer px-2'>
+            <a href={video} target='_blank' rel='noreferrer'>
+              <img
+                className='border-2 rounded h-auto'
+                style={{ borderColor: color }}
+                src={imagen}
+                alt={`Image ${id}`}
+                width={350}
+              />
+            </a>
           </li>
         ))}
     </Slider>
