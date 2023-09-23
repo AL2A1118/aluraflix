49fb8549a09caeb33519c286ef42c47d50c46b9f
Stalin Acurio <sta_vla411@hotmail.com>
2023-09-23 11:06:45 -0500

    add image and text

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,19 @@
 import Button from './Button'
+import banner from '../assets/banner.webp'
 function Banner() {
   return (
-    <section className='relative pb-4 h-[50vh] flex flex-col justify-end items-center [&>a]:w-auto [&>a]:px-10 gap-4'>
-      <div className='absolute inset-0 bg-banner bg-cover bg-center -left-4 w-screen opacity-30 z-[-1]'></div>
-      <h2 className='text-3xl text-slate-100'>Challenge React</h2>
-      <Button name='Ver' to='/' />
+    <section className='relative pb-4 md:max-h-[60vh] flex flex-col md:justify-between md:flex-row-reverse justify-end items-center [&>a]:w-auto [&>a]:px-10 gap-4 mt-4 md:mt-8'>
+      {/* <div className='absolute inset-0 bg-banner bg-contain bg-center bg-no-repeat opacity-30 z-[-1]'></div> */}
+      <img className='' src={banner} alt='' width={480} />
+      <div className='flex flex-col gap-4 max-w-[60ch] items-start [&>a]:w-auto'>
+        <h2 className='text-slate-100 text-5xl'>Alura Latam</h2>
+        <p>
+          Cursos de calidad para tu crecimiento profesional. Aprende a tu ritmo,
+          en cualquier momento y lugar. Únete a nosotros y avanza en el mundo
+          digital con Alura Latam.
+        </p>
+        <Button name='Ver' to='/' />
+      </div>
     </section>
   )
 }
